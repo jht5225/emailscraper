30ce98ca4753af05738a79c9c59f2195065980b0
Ubuntu <ubuntu@ip-172-31-61-194.ec2.internal>
2020-09-22 14:55:44 +0000

    Basic version finished

--- textanalyzer.py
+++ textanalyzer.py
@@ -3,117 +3,46 @@ from operator import itemgetter
 from textblob import TextBlob
 
 class TextAnalyzer:
-    def __init__(self, text):
+    def __init__(self, text, gazetteer=None):
         self.text = text
-        self.__process_text()
-        self.nlp = spacy.load("en_core_web_sm")
+        self.paragraphs = []
+        self.sentences = []
+        self.tokens = []
         self.__preprocess()
+        self.key_sentences = {}
+        if gazetteer:
+            self.gazetteer = gazetteer
+            self.__match_sents()
     
-
-    def __process_text(self):
-        self.__make_sentences()
-        self.__tag_and_chunk_sentences()
-
-
-    def __make_sentences(self):
-        self.sentences = []
-        for line in nltk.sent_tokenize(self.text):
-            self.sentences.append(self.__process_line(line))
-    
-    def __tag_and_chunk_sentences(self):
-        self.sentence_tags = []
-       
-        for sentence in self.sentences:
-            blob = TextBlob(sentence)
-            sentence_tags = blob.tags
-            self.sentence_tags.append(sentence_tags)
-        print(self.sentence_tags)
-
-
-    def __process_line(self,line):
-        special_chars = ['\n', '\t', u'\xa0', u'\u200b']
-        list_without_special_chars = [c for c in line if c not in special_chars]
-        list_to_line = ''.join(list_without_special_chars)
-        stripped_line = list_to_line.strip()
-        lower_case_line = stripped_line.lower()
-        return lower_case_line
-
-    
-    def get_summary_sentences(self):
-        self.__score_sents()
-        if len(self.scores) > 2:
-            tops = self.__get_dict_max(self.scores)
-        else:
-            tops = self.scores
-        sents = tops.keys()
-
-        return ' '.join(sents)
-
-    def get_summary_words(self):
-        if len(self.word_freq) > 2:
-            tops = self.__get_dict_max(self.word_freq)
-        else:
-            tops = self.word_freq
-        return tops
-
-    def search_for(self, search):
-        results = []
-        for q in search:
-            if q.lower() in self.text.lower():
-                results.append(q)
-        return results
-
-    def __get_dict_max(self, scores, N=3):
-        maxes = dict(sorted(scores.items(), key=itemgetter(1), reverse=True )[:N])
-        return maxes
-
-    def __preprocess(self):
-        self.__textsplit()
+    def __preprocess(self): 
         self.__format_text()
-        self.__count_word_frequencies()
-        self.__weight_frequencies()
-
-    def __format_text(self): 
-        formatted_article_text = re.sub('[^a-zA-Z]', ' ', self.text)
-        formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
-        self.text = formatted_article_text
-   
-    def __textsplit(self):
-        self.sentences = []
-        sentences = nltk.sent_tokenize(self.text)
-        for sent in sentences:
-            split_lines = sent.split('\n')
-            for line in split_lines:
-                self.sentences.append(line)
-
-    def __count_word_frequencies(self):
-        stopwords = nltk.corpus.stopwords.words('english')
-        self.word_freq = {}
-        for word in nltk.word_tokenize(self.text):
-            if word not in stopwords:
-                if word not in self.word_freq.keys():
-                    self.word_freq[word] = 1
-                else:
-                    self.word_freq[word] += 1
-
-    def __weight_frequencies(self):
-        max_freq = max(self.word_freq.values())
-        
-        for word in self.word_freq.keys():
-            self.word_freq[word] = (self.word_freq[word]/max_freq)
+        self.__split_lines()
     
-    def __score_sents(self):
-        scores = {}
-        for sent in self.sentences:
-            for word in nltk.word_tokenize(sent.lower()):
-                if word in self.word_freq.keys():
-                    if sent not in scores.keys():
-                        scores[sent] = self.word_freq[word]
-                    else:
-                        scores[sent] += self.word_freq[word]
-        for sent in scores.keys():
-            if scores[sent] == 1.0:
-                scores[sent] = 0 
+    def __format_text(self):
+        self.clean_text = (''.join(ch for ch in self.text if (ch.isalnum() or ch in [',', '\n', '.', '\'', '\"', ' ', ':']))).lower()
 
-        self.scores = scores
+
+    def __split_lines(self):
+        for paragraph in self.clean_text.split('\n'):
+            self.paragraphs.append(paragraph)
+            for line in paragraph.split('. '):
+                if line != '':
+                    self.sentences.append(line)
+        
+    def __match_sents(self):
+        for keyword in self.gazetteer.keys():
+            for search_val in self.gazetteer[keyword]:
+                self.key_sentences[search_val] = [sent for sent in self.sentences if re.search(search_val, sent)]
+    
+    def get_key_sentences(self):
+        keys = []
+        if self.key_sentences:
+            for keyword in self.key_sentences.keys():
+                if self.key_sentences[keyword]:
+                    keys.append({keyword: self.key_sentences[keyword]})
+        else:
+            keys = self.sentences
+        
+        return keys
+
 
